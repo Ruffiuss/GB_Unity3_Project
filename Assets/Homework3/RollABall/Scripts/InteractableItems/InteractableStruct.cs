acd066ed5fdbc439edaeb2dc002cb127f1bd60bc
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-01-19 21:57:38 +0500

    Part of Homework2 upluaded

---
+++ Assets/Homework3/RollABall/Scripts/InteractableItems/InteractableStruct.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+namespace RollABall
+{
+    [Serializable]
+    public struct InteractableStruct
+    {
+        #region Fields
+
+        public GameObject InteractableGameObject;
+        public List<GameObject> SpawnedInteractable;
+        public Vector3[] InteractableSpawn;
+
+        #endregion
+    }
+}
---
+++ Assets/Homework3/RollABall/Scripts/Interfaces/IUpdatable.cs
@@ -0,0 +1,11 @@
+﻿namespace RollABall
+{
+    internal interface IUpdatable 
+    {
+        #region Methods
+
+        void UpdateTick();
+
+        #endregion
+    }
+}
